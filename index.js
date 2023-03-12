83a8382c1a9e43bf66938a47f70337e570c5e261
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-03-12 03:31:27 +0100

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const count = document.addEventListener("click", e => {
+const count = document.addEventListener("click", (e) => {
+	sumUpEl = document.getElementById("sum-up");
 
-    sumUpEl = document.getElementById("sum-up");
-
-    const priceEl = document.getElementById("price");
+	const priceEl = document.getElementById("price");
 	const peopleEl = document.getElementById("people");
 	const tipEl = document.getElementById("tip");
 	const costEl = document.getElementById("cost");
-   
-    costEl.value = (+priceEl.value + (tipEl.value * +priceEl.value)) / +peopleEl.value
-    
-    costEl.textContent = costEl.value
+	const errorEl = document.getElementById("error");
 
-    sumUpEl=sumUpEl.style.display="block"
-})
+	if (peopleEl.value < 1) {
+		sumUpEl = sumUpEl.style.display = "none";
+		errorEl.style.display = "block";
+	}
+
+	costEl.value = +(
+		(+priceEl.value + tipEl.value * +priceEl.value) /
+		+peopleEl.value
+	).toFixed(2);
+
+	costEl.textContent = costEl.value;
+	console.log(costEl.textContent, costEl.value);
+	if (costEl.textContent == costEl.value) {
+		sumUpEl = sumUpEl.style.display = "block";
+		errorEl.style.display = "none";
+	} else {
+		sumUpEl = sumUpEl.style.display = "none";
+		errorEl.style.display = "block";
+	}
+});
